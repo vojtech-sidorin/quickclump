623b84899b63508144cafd15864269cc42d43c2a
Vojtech Sidorin <vojtech.sidorin@gmail.com>
2015-08-22 20:50:20 +0200

    Use package tempfile to prepare tmp dir

--- test_dendroclump.py
+++ test_dendroclump.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Unit tests for Dendroclump
+# Tests for Dendroclump
 #
 # Copyright 2015 Vojtech Sidorin <vojtech.sidorin@gmail.com>
 #
@@ -21,6 +21,7 @@ import sys
 import shutil
 import unittest
 import hashlib
+import tempfile
 
 import numpy as np
 # Import FITS IO.
@@ -38,53 +39,59 @@ import dendroclump as dcl
 
 
 class TestMain(unittest.TestCase):
-    """Test function main, i.e. the main Dendroclump's functionality"""
+    """Integration testing: Test function main.
 
-    TMP_DIR = "./test_tmp"
+    Let Dendroclump process sample FITS files and compare the output
+    with expected results.
+    """
+
     FIXTURES_DIR = "./fixtures"
     SAMPLE_FILES_PREFIXES = ["rand_normal",
                              "rand_uniform"]
 
     def setUp(self):
-        # Make tmp directory for test outputs.  If the directory exists mkdir
-        # will fail, the test will fail with an exception, and the directory
-        # won't be removed by tearDown.  I.e. it won't delete the directory if
-        # it already exists.
-        os.mkdir(self.TMP_DIR)
+        # Test outputs will be stored in this tmp dir.
+        self.tmpd = tempfile.mkdtemp(prefix="dendroclump-")
 
     def tearDown(self):
-        shutil.rmtree("test_tmp")
+        shutil.rmtree(self.tmpd)
 
     def test_on_sample_input_files(self):
-        """Run Dendroclump on sample files and check results."""
+        """Run main() on sample files and check results."""
         for f in self.SAMPLE_FILES_PREFIXES:
+
             # Run main() on the sample file.
             ifits = os.path.join(self.FIXTURES_DIR, f + ".fits")
-            test_ofits = os.path.join(self.TMP_DIR, f + ".clumps.fits")
-            test_otext = os.path.join(self.TMP_DIR, f + ".clumps.txt")
+            test_ofits = os.path.join(self.tmpd, f + ".clumps.fits")
+            test_otext = os.path.join(self.tmpd, f + ".clumps.txt")
             dcl.main("--ofits {ofits} --otext {otext} {ifits} --silent"
                      .format(ofits=test_ofits, otext=test_otext, ifits=ifits)
                      .split())
+
             # Compare FITS results.  Compare only data, not FITS header.
-            sample_ofits = os.path.join(self.FIXTURES_DIR, f + ".clumps.fits")
-            with fits.open(sample_ofits) as g:
-                sample_odata = g[0].data  # First HDU.
+            # NOTE: The FITS header will contain a timestamp, so we compare
+            # only data.
+            expected_ofits = os.path.join(self.FIXTURES_DIR, f + ".clumps.fits")
+            with fits.open(expected_ofits) as g:
+                expected_odata = g[0].data  # First HDU.
             with fits.open(test_ofits) as h:
                 test_odata = h[0].data  # First HDU.
-            self.assertEqual(hashlib.sha512(sample_odata).hexdigest(),
+            self.assertEqual(hashlib.sha512(expected_odata).hexdigest(),
                              hashlib.sha512(test_odata).hexdigest(),
                              msg="Data in FITS files '{0}' and '{1}' differ."
-                                 .format(sample_ofits, test_ofits))
+                                 .format(expected_ofits, test_ofits))
+
             # Compare TXT results.
-            sample_otext = os.path.join(self.FIXTURES_DIR, f + ".clumps.txt")
-            with open(sample_otext, "rb") as g:
-                sample_otext_contents = g.read()
+            expected_otext = os.path.join(self.FIXTURES_DIR, f + ".clumps.txt")
+            with open(expected_otext, "rb") as g:
+                expected_otext_contents = g.read()
             with open(test_otext, "rb") as h:
                 test_otext_contents = h.read()
-            self.assertEqual(hashlib.sha512(sample_otext_contents).hexdigest(),
-                             hashlib.sha512(test_otext_contents).hexdigest(),
-                             msg="Files '{0}' and {1} differ."
-                                 .format(sample_otext, test_otext))
+            self.assertEqual(
+                    hashlib.sha512(expected_otext_contents).hexdigest(),
+                    hashlib.sha512(test_otext_contents).hexdigest(),
+                    msg="Files '{0}' and {1} differ."
+                        .format(expected_otext, test_otext))
 
 
 class TestParseArgs(unittest.TestCase):
