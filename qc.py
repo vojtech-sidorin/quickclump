8a9390388f4d48b509aaa358c4f169ae97f0d929
Vojtech Sidorin <vojtech.sidorin@gmail.com>
2015-09-24 22:27:22 +0200

    m. Improve grammar

--- qc.py
+++ qc.py
@@ -54,7 +54,7 @@ except ImportError:
         import pyfits as fits
     except ImportError:
         sys.exit("Error: Cannot find any supported FITS IO package.  "
-                 "Do you have installed 'astropy' or 'pyfits'?")
+                 "Have you installed 'astropy' or 'pyfits'?")
 
 __version__ = "1.3-2"
 
